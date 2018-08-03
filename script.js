89e8158fbffa244c992b4ddd1b764db0bc6e5ffc
muhamedkarajic <mountgraph@gmail.com>
2018-08-03 21:07:58 +0200

    Update new

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-var overlay = document.getElementById("overlay");
+/*var overlay = document.getElementById("overlay");
 
 window.addEventListener('load', function () {
     overlay.style.display = 'none';
 })
+ */
+
+window.addEventListener('load', function() {
+    
+    var video = document.getElementById('video');
+    var preloader = document.getElementById('overlay');
+
+    function checkLoad() {
+        console.log(video.readyState)
+        if (video.readyState === 4) {
+            preloader.style.display = 'none';
+        } else {
+            setTimeout(checkLoad, 100);
+        }
+    }
+
+    checkLoad();
+}, false);

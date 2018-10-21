1b7cbe7113868cdc67ba16d2923e71d35b4d6b3f
Muhamed Karajic <35216938+muhamedkarajic@users.noreply.github.com>
2018-10-21 08:56:05 +0200

    Update logo.

--- script.js
+++ script.js
@@ -30,9 +30,6 @@ window.addEventListener('load', function () {
     
     preloader.style.background = 'none';
     document.getElementById("logo").classList.add("logo-animation");
-    if (video.paused) { 
-        document.getElementById('video-button').className = "fa fa-play"; 
-    }
 
     function checkLoad() {
         if (video.readyState === 4 || i === 60) {

fa1400267a761a7921dbdd20b43ab8f5124fab97
Muhamed Karajic <35216938+muhamedkarajic@users.noreply.github.com>
2019-02-04 06:35:17 +0100

    update

--- script.js
+++ script.js
@@ -7,10 +7,9 @@ var button = document.getElementById('video-button');
 var size = {
     width: window.innerWidth || document.body.clientWidth,
     height: window.innerHeight || document.body.clientHeight
-    
 }
 
-
+var buttonPress = false;
 var phone = false;
 if(size.width < size.height)
     phone = true;
@@ -26,7 +25,8 @@ function updateVideo(){
     if(size.width < size.height && !phone)
     {
         phone = true;
-        document.getElementById('video').src = "phone.mp4#t=0.1";
+        // document.getElementById('video').src = "phone.mp4#t=0.1";
+        document.getElementById('video').src = "intro.mp4#t=0.1";
     }    
     else if(size.width > size.height && phone)
     {
@@ -85,11 +85,16 @@ window.addEventListener('focus', playVideo, false);
 function pauseStream() 
 {
     window.removeEventListener('blur',pauseStream);
+    window.removeEventListener('focuse',playVideo);
+    //window.removeEventListener('scroll', parallex);
+
     if (video.paused) {
         playVideo();
+        buttonPress = false;
     }
     else {
         pauseVideo();
+        buttonPress = true;
     }
 }
 
@@ -117,6 +122,7 @@ var block2 = document.getElementById("block2");
 block1.style.width = "100px";
 block1.style.height = "100px";
 block1.style.left = "70%";
+block2.style.left = "15%";
 
 // block3.style.width = "25px";
 // block3.style.height = "25px";
@@ -124,36 +130,37 @@ block1.style.left = "70%";
 
 var i=0;
 
-var yScrollPosition = window.scrollY;
+var offset = window.scrollY;
 function parallex() {
-    yScrollPosition = window.scrollY
+    offset = window.scrollY;
+    console.log(offset);
+    
     CheckVideo();
-    ypos = window.pageYOffset;
-    //console.log("ypos = "+ypos + "  -> yscroll = "+yScrollPosition);
-    block1.style.transform ="translate3d(" +0 +"px,"+ yScrollPosition*(-2)+"px,0)";
-    block1.style.webkitTransform += "rotateZ("+ yScrollPosition*(.25)+"deg)";
-
-    block2.style.transform ="translate3d(" +0 +"px,"+ yScrollPosition*(-.76)+"px,0)";
-    block2.style.webkitTransform += "rotateZ("+ yScrollPosition*(-.35)+"deg)";
-    block2.style.borderRadius = yScrollPosition*(.028)+"px";
-    block1.style.borderRadius = yScrollPosition*(.028)+"px";
 
 
-    // block3.style.transform ="translate3d(" +0 +"px,"+ yScrollPosition*(-.5)+"px,0)";
-    // block3.style.webkitTransform += "rotateZ("+ yScrollPosition*(-.1)+"deg)";
+    block1.style.transform ="translate3d(" + 0 + "px," +    offset*(-2) +   "px,    0)";
+    block2.style.transform ="translate3d(" + 0 + "px," +    offset*(-.76) + "px,    0)";
+    
+    
+    
+    //block1.style.backgroundPositionY = (offset * (-2)) + "px";
 
+    //block2.style.backgroundPositionY = (offset * (-0.76)) + "px";
 
+    block1.style.webkitTransform += "rotateZ("+ offset*(.25)+"deg)";
 
+    block2.style.webkitTransform += "rotateZ("+ offset*(-.35)+"deg)";
+    block2.style.borderRadius = offset*(.028)+"px";
+    block1.style.borderRadius = offset*(.028)+"px";
 }
-window.addEventListener('scroll', parallex);
+window.addEventListener('scroll', parallex, false);
 
 
 function CheckVideo()
 {
-    console.log(yScrollPosition*2 +">"+ size.height);
-    if((yScrollPosition*2) > size.height)
+    if(offset*2 > size.height)
         pauseVideo();
-    else
+    else if(offset*2 < size.height  && !buttonPress)
         playVideo();
 }
 
